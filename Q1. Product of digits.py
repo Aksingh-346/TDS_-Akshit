778b410b2992bc37f213227286de84a934c796c3
Aksingh-346 <aksingh2122006@gmail.com>
2024-09-21 17:53:54 +0530

    Add files via upload

---
+++ Q1. Product of digits.py
@@ -0,0 +1,13 @@
+#Python
+def product(inp):
+    for duct in inp:
+        global pro
+        product = pro*int(duct)
+        pro = int(duct)
+    print(product)
+    
+while True:
+    pro = 0
+    inp = input("Enter the no.: ")
+    if 10<=int(inp)<=99:
+        product(inp)
