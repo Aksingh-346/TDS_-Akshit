7606b9261dd500d9be4ff69498a34f44e2ea7752
Aksingh-346 <aksingh2122006@gmail.com>
2024-09-23 18:20:20 +0530

    Add files via upload

---
+++ Q2. Maximize Difference!.py
@@ -0,0 +1,23 @@
+#Python
+def main(x,y):
+    n = 0
+    z = x
+    for i in range((inpy-inpx)+1):
+        if x != y:
+            a = (z-x)+(y-z)
+            z += 1
+            if a > n:
+                n = a
+        elif x == y:
+            break
+    print(n)
+
+while True:
+    inpx = int(input("Enter the 1st no.: "))
+    inpy = int(input("Enter the 2nd no.: "))
+    if (1<=inpx<=inpy<=10):
+        main(inpx,inpy)
+    else:
+        print("*Please enter the numbers b/w 1 to 10*")
+        continue
+    
